c3fe73e4a3e2e950253990602301cd3432e56c0f
Elithon Silva <elithonsilva@gmail.com>
2022-02-10 16:11:21 -0300

    remove console.log index.js l8

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${port} </h1>`));
-app.listen(port, () => console.log(`Online: ${port}`));
+app.listen(port, () => (`Online: ${port}`));
